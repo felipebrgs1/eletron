cbc7e5f9e645bb075b6a598e8d88f56335fece76
felipeb <felipeborgaco@hotmail.com>
2025-05-18 22:01:21 -0300

    titlebar final

--- src/renderer/src/components/TitleBar.tsx
+++ src/renderer/src/components/TitleBar.tsx
@@ -1,35 +1,44 @@
 import { Minus, Square, X } from 'lucide-react';
+import { Button } from './ui/button';
 
 export const TitleBar = () => {
     return (
         <div
             className='w-full h-8 bg-gray-800 flex items-center px-2 select-none'
-            style={{ WebkitAppRegion: 'drag' }}
+            style={{ WebkitAppRegion: 'drag' } as any}
         >
             <span className='text-white text-sm flex-1'>Minha App</span>
             <div
                 className='flex gap-2'
-                style={{ WebkitAppRegion: 'no-drag' }}
+                style={{ WebkitAppRegion: 'no-drag' } as any}
             >
-                {/* Botões de minimizar, maximizar, fechar */}
-                <button
+                <Button
+                    variant='ghost'
+                    size='title'
                     onClick={() => window.electronAPI.minimize()}
-                    className='text-white px-2'
+                    className='text-white hover:bg-yellow-500'
                 >
                     <Minus size={16} />
-                </button>
-                <button
+                </Button>
+                <Button
+                    variant='ghost'
+                    size='title'
                     onClick={() => window.electronAPI.maximize()}
-                    className='text-white px-2'
+                    className='text-white hover:bg-sky-500'
                 >
                     <Square size={16} />
-                </button>
-                <button
+                </Button>
+                <Button
+                    variant='ghost'
+                    size='title'
                     onClick={() => window.electronAPI.close()}
-                    className='text-white px-2'
+                    className='text-white hover:bg-red-500'
                 >
-                    <X size={16} />
-                </button>
+                    <X
+                        size={16}
+                        className=''
+                    />
+                </Button>
             </div>
         </div>
     );
